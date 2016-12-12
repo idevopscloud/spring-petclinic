eb54c6f66c2ceb619dbb5a57ca35b2081d5ececa
高正 <allen.gao@ndpmedia.com>
2016-12-12 14:16:36 +0800

    fix build.sh bug

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,6 @@
 export PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin
 export WORKDIR=$( cd ` dirname $0 ` && pwd )
 
-bash ../mvnw clean install
+cd  .. && ./mvnw clean install
 
 echo "build ok"
