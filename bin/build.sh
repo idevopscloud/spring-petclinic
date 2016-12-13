691063e1a181938d2bfb39a7a81d530f0250416e
高正 <allen.gao@ndpmedia.com>
2016-12-13 13:06:44 +0800

    do not clean when build

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,6 @@
 export PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin
 export WORKDIR=$( cd ` dirname $0 ` && pwd )
 
-cd  .. && ./mvnw clean install
+cd  .. && ./mvnw install
 
 echo "build ok"
