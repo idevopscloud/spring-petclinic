e79ac2dbdb55c66474090d666e9ef42fab60b7b3
高正 <allen.gao@ndpmedia.com>
2016-12-13 13:19:16 +0800

    install dependent package when build os+lib image

--- bin/env.sh
+++ bin/env.sh
@@ -3,5 +3,4 @@
 export PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin
 export WORKDIR=$( cd ` dirname $0 ` && pwd )
 
-apt-get update
-apt-get install -y openjdk-7-jdk
+apt-get update && apt-get install -y openjdk-7-jdk git && git clone https://github.com/idevopscloud/spring-petclinic.git && cd spring-petclinic && ./mvnw clean install
